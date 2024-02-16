7c4d4f939fdb8ca34034a104a997484159866a7b
olicamarota <157046992+olicamarota@users.noreply.github.com>
2024-02-16 14:53:36 +0100

    Versione aggiornata
    Aggiunto controlli
    Aggiunto commenti

--- Progetto/VerbScanner.py
+++ Progetto/VerbScanner.py
@@ -1,20 +1,28 @@
-import http.client
+import http.client  #Import del modulo http.client
 
-listaVerbi=['OPTIONS','PUT','HEAD','DELETE','TRACE','CONNECT','GET','POST']
+listaVerbi=['OPTIONS','PUT','HEAD','DELETE','TRACE','CONNECT','GET','POST'] #Lista di verbi HTTP
 
 host = input("Inserire l'IP del sistema target: ")
 port = input("Inserire la porta del sistema target (default '80'): ")
 path = input("Inserire il path di cui si vuole avere una lista dei metodi abilitati (default '/'): ")
 
-try:
-    connection = http.client.HTTPConnection(host, port)
-    for i in listaVerbi:
-        connection.request(i,path)
-        response=connection.getresponse()
+if (port == "" or int(port) <0 or int(port) >65535): #Se l'utente non inserisce la porta corretta, viene impostata di default la porta 80.
+    print("E' stata impostata la porta di default.\n")
+    port=80
 
-        if(response.status == 200):
+if (path == ""): #Se l'utente non inserisce il path, viene impostato di default il path '/'.
+    print("E' stato impostato il path di default.\n")
+    path="/"
+
+try:
+    connection = http.client.HTTPConnection(host, port) #Viene instaurata una connessione con la macchina target.
+    for i in listaVerbi:
+        connection.request(i,path) #Invio di una richiesta HTTP per ogni verbo della lista.
+        response=connection.getresponse() #Ricezione della risposta dal server
+
+        if(response.status == 200): #Se il metodo getresponse() ha restituito come valore "200", la connessione è andata a buon fine. 
             print("Il metodo " + i + " è abilitato.")
 
-        connection.close()
-except ConnectionRefusedError:
+        connection.close() #Chiusura della connessione
+except ConnectionRefusedError: #Gestione dell'errore "ConnectionRefused"
     print("Connessione fallita")
